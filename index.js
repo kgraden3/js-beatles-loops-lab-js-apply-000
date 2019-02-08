ea6b116b6be29011d5a23f738a38b46b093458d5
Kyle Graden <kylegraden3@gmail.com>
2019-02-08 01:09:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // add solution here
 function theBeatlesPlay (musicians,instruments) {
-  return for(musicians = 0; musicians + "plays" + instruments;musicians++    )
-const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]
+  return; for(musicians = 0; musicians + "plays" + instruments;musicians++    )
+  const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+  const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]
 }
